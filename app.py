c2b62604d7a42adcd202c622171311d162ffc80d
tonyntgit <116912787+tonyntgit@users.noreply.github.com>
2022-10-29 08:55:28 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+def handler(event, context):
+    print('Start SQS-Lamda Event:')
+    print(event)
+
+    records = event['Records']
+
+    for record in records:
+        username = record['body']        
+        user_data = github.get_user_detail(username)
+
+        print(user_data)
+
+    print('End SQS-Lamda Function')    
+
+def get_user_detail(username: str):
+    user_data = requests.get('https://api.github.com/users/%s' % username).json()
+
+    return user_data
